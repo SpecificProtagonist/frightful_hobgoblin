a23477d1d5a2844731e4f5734677f57e5ee1eddf
Vincent Junge <specificprotagonist@posteo.org>
2024-03-18 03:53:25 +0100

    use world seed by default

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,14 +1,18 @@
 #![allow(dead_code)]
+use std::fs::File;
+
 use config::*;
 use mc_gen::sim::sim;
 use mc_gen::*;
 use nanorand::*;
+use nbt::decode::read_gzip_compound_tag;
 
 fn main() {
-    let seed = std::env::args()
-        .nth(1)
-        .map(|seed| seed.parse().expect("Invalid seed"));
-    let seed = seed.unwrap_or(tls_rng().generate::<u16>() as u64);
+    let seed = match std::env::args().nth(1) {
+        Some(seed) if seed == "random" => tls_rng().generate::<u16>() as u64,
+        Some(seed) => seed.parse().expect("Invalid seed"),
+        None => get_seed(SAVE_READ_PATH),
+    };
     println!("Seed: {seed}");
     RNG.set(WyRand::new_seed(seed));
 
@@ -17,4 +21,15 @@ fn main() {
     let level = Level::new(SAVE_READ_PATH, SAVE_WRITE_PATH, area);
 
     sim(level);
+}
+
+fn get_seed(path: &str) -> u64 {
+    let nbt =
+        read_gzip_compound_tag(&mut File::open(format!("{path}/level.dat")).unwrap()).unwrap();
+    nbt.get_compound_tag("Data")
+        .unwrap()
+        .get_compound_tag("WorldGenSettings")
+        .unwrap()
+        .get_i64("seed")
+        .unwrap() as u64
 }
